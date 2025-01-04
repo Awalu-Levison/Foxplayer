a4654c4310cdc75b1a874d5681707a2dca224dde
Awalu-Levison <levisonawalu251@gmail.com>
2025-01-04 19:46:19 +0200

    Updated route links to nav menu

--- fox-webplayer/src/App.js
+++ fox-webplayer/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import NowPlaying from "./components/NowPlaying";
+import "./index.css";
+
+/* Importing the navigation menu */
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Library from "./pages/Library";
+import Playlist from "./pages/Playlist";
+import Account from "./pages/Account";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="search" element={<Search />} />
+            <Route path="library" element={<Library />} />
+            <Route path="playlist" element={<Playlist />} />
+            <Route path="account" element={<Account />} />
+          </Routes>
+        </main>
+        <NowPlaying />
+      </div>
+    </Router>
+    </>
   );
 }
 
