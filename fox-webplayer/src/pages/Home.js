25d5099fd08d4934b70cb415cc8d059ea0fd4c58
Awalu-Levison <levisonawalu251@gmail.com>
2025-01-08 07:10:24 +0200

    Add home component

--- fox-webplayer/src/pages/Home.js
+++ fox-webplayer/src/pages/Home.js
@@ -1,10 +1,26 @@
 import React from "react";
+import HeroSection from "../pages/HeroSection.js";
+import About from "../pages/About.js";
+import "../styles/About.css"
+import SearchBar from "../components/SearchBar.js";
+import Trending from "../components/Trending.js";
+import Recommendations2 from "../components/Recommendations2.js";
+import MusicPlayer from "../components/MusicPlayer.js";
 
 const Home = () => {
-    return <div>
-            <h1>Welcome to Foxplayer</h1>
-            <p>Your favorite music, all in one place!</p>
+    return (
+        <div>
+            <MusicPlayer />
+            <SearchBar />
+            <HeroSection />
+            <Recommendations2 />
+            <Trending />
+            <About />
+            {/*<Contact />*/}
         </div>
+  
+
+    );
 };
 
 export default Home;
