55cf8246151043f5415fc1272d95d7a5077e8c14
Awalu-Levison <levisonawalu251@gmail.com>
2025-01-08 07:09:24 +0200

    Add music explore section

---
+++ fox-webplayer/src/pages/Explore.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Explore = () => {
+    return <div>
+        <h1>Explore mmusic here</h1>
+        <p>Here is your song list</p>
+        </div>
+};
+
+export default Explore;
