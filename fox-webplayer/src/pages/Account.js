2ca1d14d5bd807212cda8e95d0a341bf62b767c7
Awalu-Levison <levisonawalu251@gmail.com>
2025-01-08 07:08:27 +0200

    Add account section

--- fox-webplayer/src/pages/Account.js
+++ fox-webplayer/src/pages/Account.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 const Account = () => {
-    return <div>
-        <h1>User Acoount</h1>
-        <p>This is your account</p>
-    </div>
+    return (
+        <div>
+         <h1>User Account</h1>
+         <p>Create your account</p>
+        </div>
+    );
 };
 
 export default Account;
