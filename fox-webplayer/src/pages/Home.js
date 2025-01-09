b23837af49b0c40c0b4d97a21f1636c766be2a1f
Awalu-Levison <levisonawalu251@gmail.com>
2025-01-09 05:36:04 +0200

    Updated search bar

--- fox-webplayer/src/pages/Home.js
+++ fox-webplayer/src/pages/Home.js
@@ -5,13 +5,17 @@ import "../styles/About.css"
 import SearchBar from "../components/SearchBar.js";
 import Trending from "../components/Trending.js";
 import Recommendations2 from "../components/Recommendations2.js";
-import MusicPlayer from "../components/MusicPlayer.js";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import MusicPlayer from "../components/MusicPlayer.js";
+//import { faMusic, faPlayCircle } from '@fortawesome/free-solid-svg-icons';
+
 
 const Home = () => {
     return (
         <div>
-            <MusicPlayer />
-            <SearchBar />
+           <SearchBar />
+            <FontAwesomeIcon />
+           {/*<MusicPlayer />*/}
             <HeroSection />
             <Recommendations2 />
             <Trending />
